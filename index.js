8a78ce126d0f8a5729f9bfcbb52ab8eab0295340
ArjunBiseswar <arjun.biseswar@gmail.com>
2022-01-19 12:19:48 +0100

    opdracht volledig

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 window.addEventListener('load', slideshowSetup);
 
+let slideindex = 0;
+let slides;
+
 function slideshowSetup() {
     document.getElementById('slideshow-next').addEventListener('click', slideshowNext);
+    document.getElementById('slideshow-prev').addEventListener('click', slideshowPrevious);
 }
 
 function slideshowNext() {
+    let slides = document.getElementsByClassName('slide');    
+
+    slides[slideindex].classList.remove('visible');
+
+    slideindex += 1;
+
+    if(slideindex == slides.length) {
+        slideindex = 0;
+    }
+
+    slides[slideindex].classList.add('visible');
+}
+
+function slideshowPrevious() {
     let slides = document.getElementsByClassName('slide');
 
-    slides [0].style.display = 'none';
+    slides[slideindex].classList.remove('visible');
+
+    slideindex -= 1;
+
+    if(slideindex == -1) {
+        slideindex = slides.length - 1;
+    }
+
+    slides[slideindex].classList.add('visible');
 }
